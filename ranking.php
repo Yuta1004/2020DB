014a7d7228b27889c5a5e5b6507ba81ca8a4c15b
NakagamiYuta <guguru0014@gmail.com>
2021-01-09 03:59:52 +0900

    [add] ランキングページ実装

--- ranking.php
+++ ranking.php
@@ -3,6 +3,15 @@ session_start();
 
 require "util_func.php";
 list($mylink, $hdmsg) = getUserMsg();
+
+// Get raning order by Valuations
+$dbh = getDBHandler();
+$sql = "select U.nickname as nickname, Q.question_id as question_id, Q.title as title, Q.date as date, Q.body as body, count(V.user_id) as count from Questions as Q inner join Users as U on Q.user_id=U.user_id left join Valuations as V on Q.question_id=V.question_id group by Q.question_id order by count desc, Q.date desc limit 5;";
+$v_ranking_questions = $dbh->query($sql)->fetchAll();
+
+// Get ranking order by Answer nums;
+$sql = "select U.nickname as nickname, Q.question_id as question_id, Q.title as title, Q.date as date, Q.body as body, count(A.user_id) as count from Questions as Q inner join Users as U on Q.user_id=U.user_id left join Answers as A on Q.question_id=A.question_id group by Q.question_id order by count desc, date desc limit 5;";
+$a_ranking_questions = $dbh->query($sql)->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -20,34 +29,52 @@ list($mylink, $hdmsg) = getUserMsg();
     <h1 class="title" onclick="javascript:window.location='/';return false;"><u>Study Q</u></h1>
     <h2 class="minititle">ランキング</h2>
     <hr>
-    <ul class="menubar">
-        <li class="menubar">
-            <div width="50%">
+    <ul class="menubar" style="width: 90%; margin-left: auto; margin-right: auto;">
+        <li class="menubar" style="width: 50%; margin: 0px 10px 0px 10px;">
+            <div style="width: 100%;">
                 <h2 class="minititle"><i>難易度順</i></h2>
-                <div class="listelement">
-                    <b>タイトル</b><br>
-                    投稿者名: あああ<br>
-                    投稿日時: 1970/01/01 00:00:00<br>
-                    HELP!: <i>9999</i><br><br>
-                    <u>内容</u><br>
-                    あああああああああああああああああああああああああああああああああああああ…
-                </div>
+                <?php
+                    foreach($v_ranking_questions as $question) {
+                        $question_id = $question["question_id"];
+                        $title = $question["title"];
+                        $nickname = $question["nickname"];
+                        $date = $question["date"];
+                        $body = $question["body"];
+                        $count = $question["count"];
+                        echo "<div class='listelement' style='width:70%; margin-left: auto; margin-right: auto;'>\n";
+                        echo "<h3><b><u><a href='detail.php?id=$question_id'>$title</a></u></b></h3>\n";
+                        echo "投稿者名: $nickname<br>\n";
+                        echo "投稿日時: $date<br>\n";
+                        echo "HELP!: <i>$count</i><br><br>\n";
+                        echo "<u>内容</u><br> <pre style='font-size:18px;'>$body</pre>\n";
+                        echo "</div>";
+                    }
+                ?>
             </div>
         </li>
         <li class="menubar">
-            <div style="background-color:gray; width:2px; height:500px; margin: 0px 50px 0px 50px;"></div>
+            <div class="center" style="background-color:gray; width: 2px; height:1500px;"></div>
         </li>
-        <li class="menubar">
-            <div width="50%">
+        <li class="menubar" style="width: 50%; margin: 0px 10px 0px 10px;">
+            <div style="width: 100%;">
                 <h2 class="minititle"><i>回答数</i></h2>
-                <div class="listelement">
-                    <b>タイトル</b><br>
-                    投稿者名: あああ<br>
-                    投稿日時: 1970/01/01 00:00:00<br>
-                    HELP!: <i>9999</i><br><br>
-                    <u>内容</u><br>
-                    あああああああああああああああああああああああああああああああああああああ…
-                </div>
+                <?php
+                    foreach($a_ranking_questions as $question) {
+                        $question_id = $question["question_id"];
+                        $title = $question["title"];
+                        $nickname = $question["nickname"];
+                        $date = $question["date"];
+                        $body = $question["body"];
+                        $count = $question["count"];
+                        echo "<div class='listelement' style='width:70%; margin-left: auto; margin-right: auto;'>\n";
+                        echo "<h3><b><u><a href='detail.php?id=$question_id'>$title</a></u></b></h3>\n";
+                        echo "投稿者名: $nickname<br>\n";
+                        echo "投稿日時: $date<br>\n";
+                        echo "回答数: <i>$count</i><br><br>\n";
+                        echo "<u>内容</u><br> <pre style='font-size:18px;'>$body</pre>\n";
+                        echo "</div>";
+                    }
+                ?>
             </div>
         </li>
     </ul>
