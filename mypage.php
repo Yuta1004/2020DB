47bc4e83226fb8fe75cf9726046af39468402ab6
NakagamiYuta <guguru0014@gmail.com>
2021-01-09 04:51:21 +0900

    [update] マイページの自身の投稿/回答一覧ボタンを機能するようにした

--- mypage.php
+++ mypage.php
@@ -3,6 +3,10 @@ session_start();
 
 require "util_func.php";
 requireLogin();
+
+$userid = $_SESSION["studyq_userid"];
+$query_q_list = "search.php?genre=Questions&target=user_id&keyword=$userid";
+$query_a_list = "search.php?genre=Answers&target=user_id&keyword=$userid";
 ?>
 
 <!DOCTYPE html>
@@ -17,8 +21,8 @@ requireLogin();
     <h1 class="title" onclick="javascript:window.location='/';return false;"><u>Study Q</u></h1>
     <h2 class="minititle">マイページ</h2><br><br>
     <div class="center">
-        <button type="button" onclick="javascript:window.location='search.php';return false;">投稿一覧</button><br><br><br>
-        <button type="button" onclick="javascript:window.location='search.php';return false;">回答一覧</button><br><br><br>
+        <button type="button" onclick="javascript:window.location='<?php echo $query_q_list; ?>';return false;">自身の投稿一覧</button><br><br><br>
+        <button type="button" onclick="javascript:window.location='<?php echo $query_a_list; ?>';return false;">自身の回答一覧</button><br><br><br>
         <button type="button" onclick="javascript:window.location='userinfo.php';return false;">ユーザ情報確認・変更</button><br><br><br>
         <button type="button" onclick="javascript:window.location='logout.php';return false;">ログアウト</button><br><br><br>
     </div>
