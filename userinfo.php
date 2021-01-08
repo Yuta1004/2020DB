85335a7402fd63c72a8567a73632963652d1d99d
NakagamiYuta <guguru0014@gmail.com>
2021-01-09 01:03:26 +0900

    [add] ユーザ情報表示ページのフォームに、ログイン情報を表示するようにした

--- userinfo.php
+++ userinfo.php
@@ -18,12 +18,12 @@ requireLogin();
     <h2 class="minititle">ユーザ情報</h2>
     <form class="center" style="width: 400px;">
         <div style="text-align: left;">
-            <p><b>ユーザID</b>:              <input type="text" id="userid"></p>
-            <p><b>ニックネーム</b>:           <input type="text" id="nickname"></p>
-            <p><b>パスワード</b>:             <input type="password" id="password"></p>
-            <p><b>パスワード(確認用)</b>:      <input type="password" id="password_conf"></p>
+            <p><b>ユーザID</b>:          <input type="text" name="userid" value=<?php echo $_SESSION["studyq_userid"]; ?>></p>
+            <p><b>ニックネーム</b>:       <input type="text" name="nickname" value=<?php echo $_SESSION["studyq_nickname"]; ?>></p>
+            <p><b>パスワード</b>:         <input type="password" name="password"></p>
+            <p><b>パスワード(確認用)</b>:  <input type="password" name="password_conf"></p>
         </div>
-        <input type="submit" value="変更">
+        <input type="submit" name="change" value="変更">
     </form>
 </body>
 
