8f9d9d789b655745ab25170559828c6cb5999dfe
NakagamiYuta <guguru0014@gmail.com>
2021-01-09 01:50:19 +0900

    [change] 問題投稿時にpost_confirmを経由するようにした

--- post.php
+++ post.php
@@ -21,14 +21,14 @@ list($mylink, $hdmsg) = getUserMsg();
     </header>
     <h1 class="title" onclick="javascript:window.location='/';return false;"><u>Study Q</u></h1>
     <h2 class="minititle">投稿</h2>
-    <form class="center" style="width: 45%;" method="POST" action="#">
+    <form class="center" style="width: 45%;" method="POST" action="post_confirm.php">
         <div style="text-align: left;">
             <p><b>タイトル</b>:  <input type="text" size="80%" name="title"></p>
             <p><b>問題</b></p>
             <textarea rows="10" cols="90%" name="body"></textarea>
             <p><b>ひとこと</b>:  <input type="text" size="80%" name="tweet"></p>
         </div>
-        <input type="submit" name="post" value="投稿">
+        <input type="submit" value="確認">
     </form>
 </body>
 
---
+++ post_confirm.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+require "util_func.php";
+requireLogin();
+
+list($mylink, $hdmsg) = getUserMsg();
+
+$title = htmlspecialchars($_POST["title"]);
+$body = htmlspecialchars($_POST["body"]);
+$tweet = htmlspecialchars($_POST["tweet"]);
+
+// Check
+if(!($title && $body)) error(1);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h3 style="text-align: right;"><a href=<?php echo $mylink; ?>><i><?php echo $hdmsg; ?></i></a></h3>
+    </header>
+    <h1 class="title" onclick="javascript:window.location='/';return false;"><u>Study Q</u></h1>
+    <h2 class="minititle">投稿</h2>
+    <p class="center">この内容で投稿してもよろしいですか? <b>※投稿後の削除はできません</b></p>
+    <form class="center" style="width: 45%;" method="POST" action="#">
+        <div style="text-align: left;">
+            <p><b>タイトル</b>:  <?php echo $title; ?></p>
+            <p><b>問題</b></p>  <pre><?php echo $body; ?></pre><br>
+            <p><b>ひとこと</b>:  <?php echo $tweet; ?></p>
+        </div>
+        <input type="hidden" name="title" value=<?php echo $title; ?>>
+        <input type="hidden" name="body" value=<?php echo $body; ?>>
+        <input type="hidden" name="tweet" value=<?php echo $tweet; ?>>
+        <input type="submit" name="post" value="投稿">
+    </form>
+</body>
+
+</html>
