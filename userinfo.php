54dce3331464c8e343aed4abeeb3e41749430b3f
NakagamiYuta <guguru0014@gmail.com>
2021-01-09 01:24:52 +0900

    [add] ユーザ情報更新を実装した

--- userinfo.php
+++ userinfo.php
@@ -3,6 +3,39 @@ session_start();
 
 require "util_func.php";
 requireLogin();
+
+if($_POST["change"]) {
+    $userid = htmlspecialchars($_POST["userid"]);
+    $nickname = htmlspecialchars($_POST["nickname"]);
+    $pw = $_POST["password"];
+    $pw_conf = $_POST["password_conf"];
+
+    // Check
+    if(!($userid && $nickname && $pw && $pw_conf)) error(1);
+    if($pw != $pw_conf) error(2);
+
+    // Create MySQL Connection
+    try {
+        $dbh = new PDO("mysql:dbhost=localhost;dbname=db2020;unix_socket=/tmp/mysql.sock", "db2020", "db2020");
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->query("set names utf8");
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit();
+    }
+
+    // Update user info
+    try {
+        $old_userid = $_SESSION["studyq_userid"];
+        $hashed_pw = crypt($pw, "1204chino4021");
+        $dbh->query("update Users set user_id=\"$userid\", nickname=\"$nickname\", hashed_pw=\"$pw\" where user_id=\"$old_userid\";");
+        $_SESSION["studyq_userid"] = $userid;
+        $_SESSION["studyq_nickname"] = $nickname;
+    } catch (PDOException $e) {
+        error(7);
+    }
+    header("Location: message.php?msg=情報更新に成功しました", true, 301);
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +49,7 @@ requireLogin();
 <body>
     <h1 class="title" onclick="javascript:window.location='/';return false;"><u>Study Q</u></h1>
     <h2 class="minititle">ユーザ情報</h2>
-    <form class="center" style="width: 400px;">
+    <form class="center" style="width: 400px;" method="POST", action="#">
         <div style="text-align: left;">
             <p><b>ユーザID</b>:          <input type="text" name="userid" value=<?php echo $_SESSION["studyq_userid"]; ?>></p>
             <p><b>ニックネーム</b>:       <input type="text" name="nickname" value=<?php echo $_SESSION["studyq_nickname"]; ?>></p>
