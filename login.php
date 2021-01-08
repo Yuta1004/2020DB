e82f7b930be2d69be59a00ad57df9c8fb7831971
NakagamiYuta <guguru0014@gmail.com>
2021-01-08 23:46:21 +0900

    [add] ログイン処理を実装

--- login.php
+++ login.php
@@ -1,3 +1,41 @@
+<?php
+$userid = htmlspecialchars($_POST["userid"]);
+$pw = $_POST["password"];
+
+// Login
+if($_POST["login"]) {
+    // Check
+    if(!($userid && $pw)) {
+        header("Location: error.php?errno=1", true, 301);
+        exit();
+    }
+
+    // Create MySQL Connection
+    try {
+        $dbh = new PDO("mysql:dbhost=localhost;dbname=db2020;unix_socket=/tmp/mysql.sock", "db2020", "db2020");
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->query("set names utf8");
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit();
+    }
+
+    // User valid process
+    $userinfo = $dbh->query("select nickname, hashed_pw from Users where user_id=\"$userid\";")->fetch();
+    if(!$userinfo) {
+        header("Location: error.php?errno=4", true, 301);
+        exit();
+    }
+    if($userinfo["hashed_pw"] != crypt($pw, "1204chino4021")) {
+        header("Location: error.php?errno=4", true, 301);
+        exit();
+    }
+    setcookie("studyq_userid", $userid);     // (´・ω・｀)
+    setcookie("studyq_nickname", $userinfo["nickname"]);
+    header("Location: /", true, 301);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -12,12 +50,12 @@
     </header>
     <h1 class="title" onclick="javascript:window.location='index.html';return false;"><u>Study Q</u></h1>
     <h2 class="minititle">ログイン</h2>
-    <form class="center" style="width: 400px;">
+    <form class="center" style="width: 400px;" method="POST" action="#">
         <div style="text-align: left;">
-            <p><b>ユーザID</b>:              <input type="text" id="userid"></p>
-            <p><b>パスワード</b>:             <input type="password" id="password"></p>
+            <p><b>ユーザID</b>:     <input type="text" name="userid"></p>
+            <p><b>パスワード</b>:    <input type="password" name="password"></p>
         </div>
-        <input type="submit" value="ログイン">
+        <input type="submit" name="login" value="ログイン">
     </form>
 </body>
 
